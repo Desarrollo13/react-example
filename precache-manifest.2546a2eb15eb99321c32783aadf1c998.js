9bd66e23db8c8122a30daeac7ea4d8f3d52b8950
cristian <cristianmesa13@gmail.com>
2020-09-14 17:51:05 -0300

    Updates

---
+++ precache-manifest.2546a2eb15eb99321c32783aadf1c998.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f80d8b8ed4e534c5d10515c25e82d64",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "7a9b3c1071e003d8a8bf",
+    "url": "/react-example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8442d0b148b90ac1e228",
+    "url": "/react-example/static/js/2.fa566182.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-example/static/js/2.fa566182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a9b3c1071e003d8a8bf",
+    "url": "/react-example/static/js/main.c448db5b.chunk.js"
+  },
+  {
+    "revision": "134b34beb3d787db30b9",
+    "url": "/react-example/static/js/runtime-main.7eb3cfdc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
